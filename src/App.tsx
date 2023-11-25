aa24af33ad204344996df8d608fb187fd9d5ef6f
giorgiovh <jorgevonhoroch@gmail.com>
2023-11-25 15:08:47 -0500

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, ChangeEvent } from 'react'
 import './App.css'
 
+type Time = {
+  hour: number;
+  minute: number;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [wakeTime, setWakeTime] = useState<Time>({ hour: 0, minute: 0 })
+  const [bedtime, setBedtime] = useState<Time | undefined>()
+
+  function calculateBedtime(): void {
+    if (wakeTime.hour && wakeTime.minute) {
+      const bedTimeHour = wakeTime.hour >= 8 ? wakeTime.hour - 8 : wakeTime.hour + 16;
+      const bedTimeMinute = wakeTime.minute;
+
+      setBedtime({
+        hour: bedTimeHour,
+        minute: bedTimeMinute
+      })
+    }
+  }
+
+  const handleHourChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const hour = parseInt(e.target.value, 10) || 0;
+    setWakeTime({ ...wakeTime, hour })
+  }
+
+  const handleMinuteChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const minute = parseInt(e.target.value, 10) || 0;
+    setWakeTime({ ...wakeTime, minute })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Enter your wake time</h2>
+      <input type="number" placeholder='hour' value={wakeTime.hour} onChange={handleHourChange}/>
+      <br />
+      <input type="number" placeholder='minute' value={wakeTime.minute} onChange={handleMinuteChange}/>
+      <br />
+      <button onClick={calculateBedtime} disabled={!wakeTime.hour || !wakeTime.minute}>Calculate bedtime</button>
+      {bedtime && `Your bedtime is ${bedtime.hour}:${bedtime.minute}`}
     </>
   )
 }
